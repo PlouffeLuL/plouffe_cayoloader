1c67371f751ac6d70f9380c6a7e052a903c853c7
PlouffeLuL <56097826+PlouffeLuL@users.noreply.github.com>
2022-09-23 19:50:56 -0400

    fix weird files

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version "adamant"
+
+author 'PlouffeLuL'
+description 'Cayo perico ipl loader'
+version '1.0.0'
+
+games { 'gta5'}
+lua54 'yes'
+use_experimental_fxv2_oal 'yes'
+
+client_scripts {
+    'client/*.lua'
+}
